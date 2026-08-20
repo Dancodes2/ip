2ef0d2ebe792db15035d077513e98c02f5970538
Dancodes2 <177706409+Dancodes2@users.noreply.github.com>
2026-08-20 21:13:47 +0800

    Add Task class
    Represent task descriptions and completion state so Level 3 commands can mark tasks done or not done.

---
+++ src/main/java/Task.java
@@ -0,0 +1,35 @@
+/**
+ * Represents a task that can be marked as done or not done.
+ */
+
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    // Constructor.
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    // Marks task as done.
+    public void markDone() {
+        isDone = true;
+    }
+
+    // Marks task as not done.
+    public void markUndone() {
+        isDone = false;
+    }
+
+    // Returns this task's completion status.
+    public boolean getIsDone() {
+        return isDone;
+    }
+
+    @Override
+    public String toString() {
+        String status = isDone ? "[X]" : "[ ]";
+        return status + " " + description;
+    }
+}
