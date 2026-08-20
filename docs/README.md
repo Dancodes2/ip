cb9c1e2e0201154dd88901b3da940396afef372e
Dancodes2 <177706409+Dancodes2@users.noreply.github.com>
2026-08-20 18:49:34 +0800

    Rename Duke to SlotBot

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# SlotBot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/SlotBot.java
@@ -0,0 +1,13 @@
+/**
+ * The main entry point for SlotBot.
+ */
+public class SlotBot {
+    public static void main(String[] args) {
+        String banner = "  ____  _       _   ____        _   \n"
+                + " / ___|| | ___ | |_ | __ )  ___ | |_ \n"
+                + " \\___ \\| |/ _ \\| __||  _ \\ / _ \\| __|\n"
+                + "  ___) | | (_) | |_ | |_) | (_) | |_ \n"
+                + " |____/|_|\\___/ \\__||____/ \\___/ \\__|\n";
+        System.out.println(banner);
+    }
+}
