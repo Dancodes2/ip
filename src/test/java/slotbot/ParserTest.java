8f3d9383811a41f1b03eff5734465d142ee6b25b
Dancodes2 <177706409+Dancodes2@users.noreply.github.com>
2026-08-28 03:43:09 +0800

    Add initial ParserTest JUnit tests

--- src/test/java/slotbot/ParserTest.java
+++ src/test/java/slotbot/ParserTest.java
@@ -1,4 +1,25 @@
 package slotbot;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ParserTest {
+
+    @Test
+    public void parseTaskNumber_validFirstTask_returnsZero() throws SlotBotException {
+        assertEquals(0, Parser.parseTaskNumber("mark 1", 3));
+    }
+
+    @Test
+    public void parseTaskNumber_validLastTask_returnsLastIndex() throws SlotBotException {
+        assertEquals(2, Parser.parseTaskNumber("mark 3", 3));
+    }
+
+    @Test
+    public void parseTaskNumber_zeroTaskNumber_exceptionThrown() {
+        assertThrows(SlotBotException.class,
+                () -> Parser.parseTaskNumber("mark 0", 3));
+    }
 }
